44c10829b63d553a3519dc408a10d1df88074895
KrisztianKardos <frekazoid85@gmail.com>
2017-05-13 20:50:09 +0200

    headers.py created for headers

---
+++ headers.py
@@ -0,0 +1,27 @@
+HEADERS = {
+        "Menu1": [
+                "First name",
+                "Last name"
+                ],
+        "Menu2": ["Nicknames"],
+        "Menu3": [
+                "Full name",
+                "Phone number"
+                ],
+        "Menu4": [
+                "Full name",
+                "Phone number"
+                ],
+        "Menu5": [
+                "Id",
+                "First name",
+                "Last name",
+                "Phone number",
+                "Email",
+                "Application code"
+                ],
+        "Menu6": [
+                "Full name",
+                "Phone number"
+                ]
+        }
