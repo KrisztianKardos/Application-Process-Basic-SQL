ecf1ef3a384a33c01d29017e55fb3f17899b5fc9
KrisztianKardos <frekazoid85@gmail.com>
2017-05-28 19:43:48 +0200

    main.py: all_school  function defined

--- Part II/main.py
+++ Part II/main.py
@@ -24,5 +24,18 @@ def mentors():
     return render_template('mentors.html', table_headers=table_headers, mentors_data=mentors_data)
 
 
+@app.route('/all-school')
+def all_school():
+    table_headers = [
+                    'First name',
+                    'Last name',
+                    'School name',
+                    'Country'
+                    ]
+    query = all_school_query
+    all_school_data = query_manager(query, return_data='all_data')
+    return render_template('all-school.html', table_headers=table_headers, all_school_data=all_school_data)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
