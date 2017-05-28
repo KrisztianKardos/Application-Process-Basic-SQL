a5d0b523513c786c56202f60282bc373df522b13
KrisztianKardos <frekazoid85@gmail.com>
2017-05-28 19:13:38 +0200

    main.py: index, mentors functions defined

--- Part II/main.py
+++ Part II/main.py
@@ -1,3 +1,21 @@
-import flask from Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request
 import psycopg2
-from querry import *
+from queries import *
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/mentors')
+def mentors():
+    table_headers = [
+                    'First name',
+                    'Last name',
+                    'School name',
+                    'Country'
+                    ]
+    query = mentors_query
+    mentors_data = query_manager(query, return_data='all_data')
+    return render_template('mentors.html')
